01559ac917398ae1d58f767cf984353a77453bf2
EscAdn <adan.imh@gmail.com>
2025-10-10 16:10:58 -0600

    Actualizar la configuración de relaciones en el servicio de permisos de asistencia para incluir la relación del empleado.

--- src/attendance-permission/attendance-permission.service.ts
+++ src/attendance-permission/attendance-permission.service.ts
@@ -206,7 +206,9 @@ export class AttendancePermissionService {
 
       if (relations) {
         options.relations = {
-          employees: true,
+          employees: {
+            employee: true,
+          },
         };
       }
 
